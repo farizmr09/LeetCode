e9b34a2edc17743923655f139ca4d7adf3ae91e4
farizmr09 <90253110+farizmr09@users.noreply.github.com>
2024-04-13 00:31:20 +0700

    Time: 65 ms (5.26%), Space: 17 MB (7.14%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,21 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dict={1:"I",4:"IV",5:"V",9:"IX",10:"X",40:"XL",50:"L",90:"XC",100:"C",400:"CD",500:"D",900:"CM",1000:"M"}
+        holder = []
+        def roman(num):
+            for el in reversed(dict):
+                if num == el:
+                    holder.append(dict[el])
+                    num = 0
+                    break
+                if num > el:
+                    holder.append(int(num/el) * dict[el])
+                    num = num - el * int(num/el)
+                    print(num, el)
+                    break
+            if(num == 0):
+                return num
+            return roman(num)
+
+        roman(num)
+        return("".join(holder))
