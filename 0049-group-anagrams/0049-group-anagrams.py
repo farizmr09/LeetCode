ac8327f500ed23202d410a6bef8b34518a81e244
farizmr09 <90253110+farizmr09@users.noreply.github.com>
2024-04-17 02:40:55 +0700

    Time: 94 ms (40.99%), Space: 22 MB (25.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,8 +3,8 @@ class Solution:
         d = {}
         for w in strs:
             count = [0] * 26
-            for c in set(w):
-                count[ord(c) - ord('a')] = w.count(c)
+            for c in w:
+                count[ord(c) - ord('a')] += 1
             if d.get(tuple(count)) == None:
                 d[tuple(count)] = [w]
                 continue
