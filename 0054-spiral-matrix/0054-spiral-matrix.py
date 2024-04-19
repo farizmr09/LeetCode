6e65218bc427a82045a2abd0bef5242b0f17a6af
farizmr09 <90253110+farizmr09@users.noreply.github.com>
2024-04-19 15:35:52 +0700

    Time: 27 ms (94.07%), Space: 16.5 MB (60.70%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        spiral = []
+        try:
+            while len(matrix):
+                # print(spiral, matrix)
+                for i in matrix[0]:
+                    spiral.append(i)
+                matrix.remove(matrix[0])
+                for i in range(len(matrix)):
+                    spiral.append(matrix[i][-1])
+                    matrix[i].remove(matrix[i][-1])
+                for i in reversed(matrix[-1]):
+                    spiral.append(i)
+                matrix.remove(matrix[-1])
+                for i in range(len(matrix) - 1, 0, -1):
+                    spiral.append(matrix[i][0])
+                    matrix[i].remove(matrix[i][0])
+                
+        except:
+            pass
+        
+        return(spiral)
