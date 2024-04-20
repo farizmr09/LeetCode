a57dc977fb6b379eba7189b6a1256fae9c641dac
farizmr09 <90253110+farizmr09@users.noreply.github.com>
2024-04-20 17:06:47 +0700

    Time: 89 ms (74.84%), Space: 16.6 MB (63.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,26 +1,16 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(len(board)):
-            cX = Counter(board[i])
-            cY = Counter()
-            cX.pop(".")
-            if len(cX.values()) > 0 and max(cX.values()) > 1:
-                return False
-            for j in range(len(board)):
-                if board[j][i] != ".":
-                    cY.update(board[j][i])
-                if len(cY.values()) > 0 and max(cY.values()) > 1:
+        x = defaultdict(set)
+        y = defaultdict(set)
+        s = defaultdict(set)
+        
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in x[i] or board[i][j] in y[j] or board[i][j] in s[i // 3, j // 3]:
                     return False
-        
-        for i in range(0, 9, 3):
-            for j in range(0, 9, 3):
-                c = Counter()
-                for x in range(i, i + 3):
-                    for y in range(j, j + 3):
-                        if board[x][y] != ".":
-                            c.update(board[x][y])
-                        if len(c.values()) > 0 and max(c.values()) > 1:
-                            return False
-                
-        
+                x[i].add(board[i][j])
+                y[j].add(board[i][j])
+                s[i // 3, j // 3].add(board[i][j])
         return True
