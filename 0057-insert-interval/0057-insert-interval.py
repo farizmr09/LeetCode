d2dedd10bbc75b47e3e94eec71ffc44c5ba367c0
farizmr09 <90253110+farizmr09@users.noreply.github.com>
2024-04-23 12:25:26 +0700

    Time: 79 ms (32.91%), Space: 20 MB (37.10%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -10,7 +10,6 @@ class Solution:
                 res.append(intervals[i])
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-                print(newInterval)
 
         res.append(newInterval)
         return(res)
